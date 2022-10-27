4726ad952eddc4067ac7ce595f798632aa04c0f7
Ansen123 <ansenfp@gmail.com>
2022-10-27 14:23:45 +0530

    first commit

--- src/main/java/com/nestdigital/nestApp/Controller/SecurityController.java
+++ src/main/java/com/nestdigital/nestApp/Controller/SecurityController.java
@@ -1,4 +1,30 @@
 package com.nestdigital.nestApp.Controller;
 
+import com.nestdigital.nestApp.Dao.SecurityDao;
+import com.nestdigital.nestApp.Model.AdminModel;
+import com.nestdigital.nestApp.Model.SecurityModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class SecurityController {
+    @Autowired
+    private SecurityDao dao;
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/Sadd",consumes = "application/json",produces = "application/json")
+    public String AddSecurity(@RequestBody SecurityModel adds){
+        dao.save(adds);
+        return "{Status:Success}";
+    }
+    @CrossOrigin(origins = "*")
+    @GetMapping("/viewSecurty")
+    public List<SecurityModel> viewS(){
+        return (List<SecurityModel>) dao.findAll();
+    }
+
+
+
+
 }
--- src/main/java/com/nestdigital/nestApp/Model/SecurityModel.java
+++ src/main/java/com/nestdigital/nestApp/Model/SecurityModel.java
@@ -1,4 +1,79 @@
 package com.nestdigital.nestApp.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Security")
 public class SecurityModel {
+    @Id
+    @GeneratedValue
+    private int id;
+    private int sCode;
+    private String sName;
+    private String sAdd;
+    private String sPhone;
+    private String sDate;
+
+    public SecurityModel(int id, int sCode, String sName, String sAdd, String sPhone, String sDate) {
+        this.id = id;
+        this.sCode = sCode;
+        this.sName = sName;
+        this.sAdd = sAdd;
+        this.sPhone = sPhone;
+        this.sDate = sDate;
+    }
+
+    public SecurityModel() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getsCode() {
+        return sCode;
+    }
+
+    public void setsCode(int sCode) {
+        this.sCode = sCode;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+
+    public String getsAdd() {
+        return sAdd;
+    }
+
+    public void setsAdd(String sAdd) {
+        this.sAdd = sAdd;
+    }
+
+    public String getsPhone() {
+        return sPhone;
+    }
+
+    public void setsPhone(String sPhone) {
+        this.sPhone = sPhone;
+    }
+
+    public String getsDate() {
+        return sDate;
+    }
+
+    public void setsDate(String sDate) {
+        this.sDate = sDate;
+    }
 }
