6966f6de304f7e020776ea582c8feedfca06c983
Ansen123 <ansenfp@gmail.com>
2022-10-31 22:22:56 +0530

    Employee view And aproved

--- src/main/java/com/nestdigital/nestApp/Dao/LeaveDao.java
+++ src/main/java/com/nestdigital/nestApp/Dao/LeaveDao.java
@@ -12,8 +12,7 @@ public interface LeaveDao extends CrudRepository<LeaveModel,Integer> {
     @Modifying
     @Query(value = "UPDATE `leaves` SET `status`= :status WHERE id = :id",nativeQuery = true)
     void changeStatusOfLeave(int id,int status);
-    @Query(value = "SELECT l.`id`, l.`status`, l.`apply_date`, l.`emp_id`, l.`leave_date`, l.`type`," +
-            " l.`description`,e.name FROM `leaves` l JOIN employees e ON l.emp_id = e.id",nativeQuery = true)
+    @Query(value = "SELECT l.`id`, l.`status`, l.`apply_date`, l.`emp_id`, l.`leave_date`,e.e_name FROM `leaves` l JOIN employee_tb e ON l.emp_id=e.emp_id"  ,nativeQuery = true)
     List<Map<String,String>> viewAllLeave();
 
     @Query(value = "SELECT l.`id`, l.`status`, l.`apply_date`, l.`emp_id`, l.`leave_date`, l.`type`," +
